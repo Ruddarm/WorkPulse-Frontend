fe2d618ef43cdce887385f1ae15e8c7a90697afb
Ruddarm <ruddarmmaurya@gmail.com>
2025-03-16 18:26:09 +0530

    Inistialise react js

---
+++ workpulse/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      {/* Body Container */}
+      <div>
+        {/* Header */}
+        <div></div>
+
+        {/* Main Content */}
+        <div>
+          {/* Option menu */}
+          <div></div>
+          {/* Dashboard */}
+          <div> </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
